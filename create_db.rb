2bf77f89bf37f5475972c29524c614a7d69eebbf
Leonid Shevtsov <leonid@shevtsov.me>
2010-03-13 01:22:43 +0200

    initial commit

---
+++ create_db.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'sequel'
+
+db = Sequel.sqlite('db/shortener.db')
+
+db.create_table :urls do
+  primary_key :id
+  String :url
+  String :slug
+  Time :created_at
+end
+
+db.create_table :images do
+  primary_key :id
+  String :filename
+  String :slug
+  String :caption
+  Time :created_at
+end
+
+db.create_table :url_hits do
+  primary_key :id
+  foreign_key :url_id, :urls
+  String :ip, :size => 15
+  Time :visited_at
+end
+
+db.create_table :image_hits do
+  primary_key :id
+  foreign_key :image_id, :images
+  Time :visited_at
+  String :ip, :size => 15
+end
+
---
+++ shortener.rb
@@ -0,0 +1,105 @@
+require 'digest/md5'
+require 'rubygems'
+require 'sinatra'
+require 'sequel'
+require 'active_support'
+
+AUTH_USER = 'admin'
+AUTH_PASSWORD_HASH = '72b155508c6b12d17828d20d53662080'
+AUTH_SALT = 'sgawdgbsdghfsghdf'
+
+DB = Sequel.sqlite('db/shortener.db')
+
+helpers do
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Admin")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && 
+      @auth.basic? && 
+      @auth.credentials && 
+      @auth.credentials[0] == AUTH_USER && 
+      Digest::MD5.hexdigest(@auth.credentials[1]+AUTH_SALT) == AUTH_PASSWORD_HASH
+  end
+end
+
+get '/' do
+  haml :index
+end
+
+get '/s/:slug' do
+  if @record = DB[:urls].where(:slug => params[:slug]).first
+    #TODO stats
+    redirect record[:url], 301
+  else
+    error 404
+  end
+end
+
+get '/stats/s/:slug' do
+  if record = DB[:urls].where(:slug => params[:slug]).first
+    haml :url_stats, :locals => {:record => record}
+  else
+    error 404
+  end
+end
+
+get '/i/:slug' do
+  "get uploaded image"
+end
+
+get '/stats/i/:slug' do
+end
+
+get '/shorten' do
+  protected!
+  haml :shorten
+end
+
+post '/shorten' do
+  unless params[:url].blank?
+    canonical_url = URI.parse(params[:url]).to_s
+    if record = DB[:urls].where(:url => canonical_url).first
+      redirect "/stats/s/#{record[:slug]}", 303
+    else
+      #generate slug
+      begin
+        slug = rand(36**3).to_s(36) # 50k urls should be enough for anyone
+      end while DB[:urls].where(:slug => slug).first
+      DB[:urls].insert(:url => canonical_url, :slug => slug)
+      redirect "/stats/s/#{slug}", 303
+    end
+  else
+    haml :shorten
+  end
+end
+
+get '/stats' do
+  protected!
+  "Stats"
+end
+
+get '/upload' do
+  protected!
+  haml :upload
+end
+
+post '/upload' do
+  protected!
+  if params[:file] && params[:file][:tempfile]
+    tmpfile = params[:file][:tempfile]
+    tmpfile.read
+  else
+    haml :upload
+  end
+end
+
+get '/images' do
+  protected!
+  haml :images
+end
